ab9bc5786cfb5667c01ca1449642690dae3987c3
Stewart Lyall <s.lyall@doxter.de>
2015-12-21 11:03:05 +0100

    added new titleize method - not yet used

--- titleize.rb
+++ titleize.rb
@@ -3,3 +3,9 @@ class String
     self.split(" ").map{|word| word.capitalize}.join(" ")
   end
 end
+
+def titleize(str)
+  str.capitalize.split.map do |word|
+    %w{and the}.include?(word.downcase) ? word : word.capitalize
+  end.join(' ')
+end
